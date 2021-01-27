bac114ff69731ca89a04280f964f2f4e0881b80a
biencos <sewerynbienko@gmail.com>
2021-01-27 12:41:10 +0100

    Add load_register() and register()

--- sender/app.py
+++ sender/app.py
@@ -3,8 +3,9 @@ import logging
 
 from dotenv import load_dotenv
 from redis import Redis
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request, flash, url_for
 from flask_session import Session
+from bcrypt import hashpw, gensalt, checkpw
 
 
 load_dotenv()
@@ -33,14 +34,52 @@ def load_home():
 # REGISTER
 @app.route('/sender/register')
 def load_register():
-    # TODO
-    return
+    return render_template("register.html")
 
 
 @app.route('/sender/register', methods=['POST'])
 def register():
-    # TODO
-    return
+    if not request.form.get('firstname') or not request.form.get('lastname') or not request.form.get('username') or not request.form.get('email') or not request.form.get('password') or not request.form.get('password1') or not request.form.get('address'):
+        flash("Aby zarejestrować się, musisz wypełnić wszystkie pola!")
+
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    password1 = request.form.get('password1')
+    address = request.form.get('address')
+
+    if len(username) < 3 or len(username) > 12:
+        flash("Nazwa użytkownika może mieć od 3 do 12 znaków")
+    if len(password) < 8:
+        flash("Hasło musi mieć conajmniej 8 znaków")
+    if password != password1:
+        flash("Podane hasła nie pasują do siebie!")
+        response = make_response('', 302)
+        response.headers['Location'] = url_for('load_register')
+        return response
+
+    if firstname and lastname and username and email and password and password1 and address:
+        if db.hexists(f"user:{username}", "password"):
+            flash(f"Nazwa użytkownika: {username} jest zajęta")
+            response = make_response('', 302)
+            response.headers['Location'] = url_for('load_register')
+            return response
+
+        password = password.encode()
+        salt = gensalt(6)
+        hashed = hashpw(password, salt)
+
+        db.hset(f"user:{username}", "firstname", firstname)
+        db.hset(f"user:{username}", "lastname", lastname)
+        db.hset(f"user:{username}", "email", email)
+        db.hset(f"user:{username}", "password", hashed)
+        db.hset(f"user:{username}", "address", address)
+
+        response = make_response('', 302)
+        response.headers['Location'] = url_for('load_login')
+        return response
 
 
 # LOGIN
