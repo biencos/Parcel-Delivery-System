9293b4bd0446f4dc971dfb1c768c94cb02b00ce6
biencos <sewerynbienko@gmail.com>
2021-01-27 13:34:27 +0100

    Add load_login() and login()

--- sender/app.py
+++ sender/app.py
@@ -1,11 +1,13 @@
 import os
 import logging
+from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
 from redis import Redis
-from flask import Flask, render_template, make_response, request, flash, url_for
+from flask import Flask, render_template, make_response, request, flash, url_for, session
 from flask_session import Session
 from bcrypt import hashpw, gensalt, checkpw
+import jwt
 
 
 load_dotenv()
@@ -85,15 +87,70 @@ def register():
 # LOGIN
 @app.route('/sender/login')
 def load_login():
-    # TODO
-    return
+    return render_template("login.html")
 
 
 @app.route('/sender/login', methods=['POST'])
 def login():
-    # TODO
-    return
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username:
+        flash("Pole nazwa użytkownika jest puste!")
+        response = make_response('', 302)
+        response.headers['Location'] = url_for('load_login')
+        return response
+    if not password:
+        flash("Pole hasło jest puste!")
+        response = make_response('', 302)
+        response.headers['Location'] = url_for('load_login')
+        return response
+
+    if verify(username, password):
+        flash(f"Witaj z powrotem {username}")
+        session["username"] = username
+        session["logged-at"] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+        token = generate_jwt_token(username)
+        logging.info(f"Token: {token}")
+
+        response = make_response('', 302)
+        response.headers['Location'] = url_for('load_dashboard')
+        return response
+    else:
+        flash("Nieprawidłowe dane logowania!")
+        response = make_response('', 302)
+        response.headers['Location'] = url_for('load_login')
+        return response
+
+
+def verify(username, password):
+    p = password.encode()
+    h = db.hget(f"user:{username}", "password")
+
+    if h:
+        return checkpw(p, h)
+    else:
+        return False
 
 
 if __name__ == '__main__':
     app.run()
+
+
+def generate_jwt_token(username):
+    USERTYPE = "sender"
+    HOW_MANY_MINUTES_VALID = 10
+    JWT_KEY = os.getenv("JWT_PRIVATE_KEY")
+    if not username or not JWT_KEY:
+        return ''
+
+    experience_date = datetime.utcnow() + timedelta(minutes=HOW_MANY_MINUTES_VALID)
+    return jwt.encode({'username': username, 'exp': experience_date, 'usertype': USERTYPE}, JWT_KEY, algorithm='HS256')
+    # return jwt.encode({'username': username, 'exp': experience_date, 'usertype': USERTYPE}, JWT_KEY, algorithm='HS256').decode()
+
+
+def generate_headers(token):
+    headers = {}
+    headers["Authorization"] = f"Bearer {token}"
+    return headers
